9864f2846df350fdf27448cbd387e13abb0b5bac
KateKashko <107617602+KateKashko@users.noreply.github.com>
2024-04-10 20:38:27 +0300

    Add Mock Posts data for FeedView

--- Targets/SwiftBuddiesFeed/Sources/Post.swift
+++ Targets/SwiftBuddiesFeed/Sources/Post.swift
@@ -12,6 +12,41 @@ struct Post: Identifiable, Hashable, Codable {
     let id: String
     let authorId: String
     let likeCount: Int
+    let commentsCount: Int
     let content: String
-    let imageUrl: String
+    let imageUrl: String?
+    var user: User?
+}
+
+extension Post {
+    static var MOCK_POSTS: [Post] = [
+        .init(
+            id: NSUUID().uuidString,
+            authorId: NSUUID().uuidString,
+            likeCount: 98,
+            commentsCount: 10,
+            content: "Lets do it...",
+            imageUrl: "example",
+            user: User.MOCK_USERS[0]
+        ),
+        .init(
+            id: NSUUID().uuidString,
+            authorId: NSUUID().uuidString,
+            likeCount: 123,
+            commentsCount: 15,
+            content: "Hello everyone",
+            imageUrl: "",
+            user: User.MOCK_USERS[0]
+        ),
+        .init(
+            id: NSUUID().uuidString,
+            authorId: NSUUID().uuidString,
+            likeCount: 120,
+            commentsCount: 20,
+            content: "Our first steps in project...",
+            imageUrl: "logo",
+            user: User.MOCK_USERS[0]
+        )
+        
+    ]
 }
